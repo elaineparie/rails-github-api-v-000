a1fffc51ae8235458eeb732f29a922dffa1e3aac
github username <github email address>
2018-09-08 11:28:08 -0400

    working

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,6 +1,15 @@
 class RepositoriesController < ApplicationController
-  
+
   def index
   end
+
+  def create
+  resp = Faraday.post("https://api.github.com/repos") do |req|
+    req.params['oauth_token'] = session[:token]
+    req.params['name'] = params[:name]
+  end
+
+  redirect_to repositories_path
+end
 
 end
